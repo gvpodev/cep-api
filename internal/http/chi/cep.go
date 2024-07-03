7cec8e40c070a3b4473dd3bc83729890e9a1282c
Gabriel Vasconcellos Pimentel de Oliveira <gabriel.pimentel@stone.com.br>
2024-07-03 09:29:28 -0300

    feat: add chi impl

--- internal/http/chi/cep.go
+++ internal/http/chi/cep.go
@@ -1,1 +1,38 @@
 package chi
+
+import (
+	"cep-api/internal/handler"
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func Handlers(h *handler.CEPHandler) http.Handler {
+	r := chi.NewRouter()
+	r.Get("/health", healthHandler)
+	r.Get("/cep/{cepValue}", func(w http.ResponseWriter, r *http.Request) {
+		getCEP(h, w, r)
+	})
+
+	return r
+}
+
+func getCEP(h *handler.CEPHandler, w http.ResponseWriter, r *http.Request) {
+	cep := chi.URLParam(r, "cepValue")
+	result := h.GetCEP(cep)
+	if result.Fail != nil {
+		http.Error(w, result.Fail.Err.Error(), result.Fail.StatusCode)
+		return
+	}
+
+	err := json.NewEncoder(w).Encode(result.Data)
+	if err != nil {
+		http.Error(w, "Internal error: Encoding.", http.StatusInternalServerError)
+		return
+	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("App is healthy"))
+	w.WriteHeader(http.StatusOK)
+}
